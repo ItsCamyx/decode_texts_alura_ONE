d53be5557a3281551b9611f270647a1e9eb54e3c
ItsCamyx <96155623+ItsCamyx@users.noreply.github.com>
2022-08-06 00:21:20 -0300

    create base of code

---
+++ src/js/script.js
@@ -0,0 +1,5 @@
+const container = document.getElementById("main-container");
+const div1 = document.createElement("div");
+div1.classList.add("container-logo");
+div1.innerHTML = "let's go";
+container.appendChild(div1);
