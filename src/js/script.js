38e31e5c8158d904429d8acf4e7f49693d7ba002
ItsCamyx <96155623+ItsCamyx@users.noreply.github.com>
2022-08-07 01:53:53 -0300

    mobile style done

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,55 @@
+//pegando elemento principal
 const container = document.getElementById("main-container");
-const div1 = document.createElement("div");
-div1.classList.add("container-logo");
-div1.innerHTML = "let's go";
-container.appendChild(div1);
+//criando container
+const titleContainer = document.createElement("div");
+//adicionando classe
+titleContainer.classList.add("container-logo");
+//criando tag img
+const logoImg = document.createElement("img");
+logoImg.classList.add("logo");
+//colocando caminho pro img
+logoImg.src = "./assets/logo.svg";
+//adicionando texto alternativo pro img
+logoImg.alt = "Logo Alura";
+titleContainer.appendChild(logoImg);
+container.appendChild(titleContainer);
+
+const middleContainer = document.createElement("div");
+container.appendChild(middleContainer);
+middleContainer.classList.add("middle-container");
+const inputText = document.createElement("textarea");
+inputText.classList.add("input-text-encrypt");
+inputText.placeholder = "Digite seu texto aqui";
+const containerImg = document.createElement("div");
+containerImg.classList.add("container-img");
+const attencionImg = document.createElement("img");
+attencionImg.classList.add("attencion-img");
+attencionImg.src = "./assets/attencion.svg";
+const titleAttencion = document.createElement("div");
+titleAttencion.classList.add("attencion-title");
+titleAttencion.innerHTML = "Apenas letras minúsculas e sem acento.";
+const containerBtn = document.createElement("div");
+containerBtn.classList.add("container-btn");
+const encryptBtn = document.createElement("button");
+encryptBtn.setAttribute("id", "encrypt-btn");
+encryptBtn.innerText = "Criptografar";
+const decryptBtn = document.createElement("button");
+decryptBtn.setAttribute("id", "decrypt-btn");
+decryptBtn.innerText = "Descriptografar";
+containerBtn.append(encryptBtn, decryptBtn);
+containerImg.append(attencionImg, titleAttencion);
+middleContainer.append(inputText, containerImg, containerBtn);
+const containerTextResult = document.createElement("div");
+containerTextResult.classList.add("container-text-result");
+container.append(containerTextResult);
+const boxText = document.createElement("div");
+boxText.classList.add("text-result");
+containerTextResult.append(boxText);
+const textResult = document.createElement("div");
+textResult.classList.add("not-found-text");
+textResult.innerText = "Nenhuma mensagem encontrada";
+const messageRequired = document.createElement("div");
+messageRequired.classList.add("message-required-text");
+messageRequired.innerText =
+  "Digite um texto que você deseja criptografar ou descriptografar.";
+boxText.append(textResult, messageRequired);
